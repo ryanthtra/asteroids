d16f56c20ae430207a2d5e195592987abe730d1b
ryanthtra <rtransfig@gmail.com>
2025-06-01 01:02:48 -0400

    B: added CircleShape and Player classes.  Drawing player triangle on the black window.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -13,13 +14,23 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0; # delta time
+
+    # Initialize player
+    my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+
+        my_player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
